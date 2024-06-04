7576871328eeabf6e2cb83cb3a573b1dfcf6f1d6
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-06-03 20:11:44 -0600

    version inicial main

---
+++ main.py
@@ -0,0 +1,11 @@
+# "Conjetura de goldbach"
+
+# Todo numero par, puede ser representado como la suma de dos numeros primos
+
+# Encuentre todas las parejas de numeros primos para un valor X
+# entrada
+x = 10
+# salida
+lista = [[3, 7], [5, 5], [7, 3]]
+
+print(lista)
