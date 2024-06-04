7d28f352c1837a10d044f57cd5bae3a987076c16
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-06-03 20:13:33 -0600

    quitamos comentarios en main

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 
 # Todo numero par, puede ser representado como la suma de dos numeros primos
 
-# Encuentre todas las parejas de numeros primos para un valor X
-# entrada
+
 x = 10
-# salida
+
 lista = [[3, 7], [5, 5], [7, 3]]
 
 print(lista)
