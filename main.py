1a13ad8047a83346e4bd9845b73e9529d6e39f05
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-06-04 19:14:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
 # "Conjetura de goldbach"
 
-# Todo numero par, puede ser representado como la suma de dos numeros primos
+# función para detectar numeros pares
+def is_even(value):
+    resultado = False
+    if value % 2 == 0:
+        resultado = True
+    return resultado
 
 
-x = 10
+def is_prime(value):
+    resultado = False
+    if value == 2:
+        resultado = True
+        return resultado
+    else:
+        for num in range(2, value):
+            if value % num == 0:
+                resultado = False
+                break
+            else:
+                resultado = True
+    return resultado
 
-lista = [[3, 7], [5, 5], [7, 3]]
 
-print(lista)
+def get_primes(value):
+    resultado = []
+    for num in range(2, value):
+        if is_prime(num):
+            resultado.append(num)
+    return resultado
+
+
+def goldbach_conjeture(value):
+    primes_list = get_primes(value)
+    goldbach_list = []
+    for num in primes_list:
+        difference = value - num
+        if is_prime(difference):
+            for num2 in primes_list:
+                if difference + num2 == value:
+                    goldbach_list.append([difference, num2])
+    return goldbach_list
+
+
+print(goldbach_conjeture(100))
