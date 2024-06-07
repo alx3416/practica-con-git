02f06d54efc10832abbab7efba29d771d82df1e1
cartografunk <82617147+cartografunk@users.noreply.github.com>
2024-06-06 20:12:28 -0600

    addTriangulosxAsteriscos
    Agregué mejora para boundary cases y agregué 3 unit tests

--- C1_TrianguloAsteriscos.py
+++ C1_TrianguloAsteriscos.py
@@ -1,8 +1,8 @@
 def TrianguloAsterisco(i, t=0):
-    if i == 0:
-        return 0
+    i = int(i)
+
+    if i <= 0:
+        return t
     else:
         print(' ' * ( i + 1 ) + '*' * ( t * 2 + 1 ))
         return TrianguloAsterisco( i - 1, t + 1 )
-
-print(TrianguloAsterisco(5))
--- testings.py
+++ testings.py
@@ -1,5 +1,6 @@
 import pytest
 import areas as ars
+import C1_TrianguloAsteriscos as C1
 
 
 def test_get_square_area_1():
@@ -11,3 +12,12 @@ def test_get_square_area_2():
 
 def test_get_square_area_3():
     assert ars.get_square_area(0) == 0
+
+def test_get_TrianguloAsteriscos_1():
+    assert C1.TrianguloAsterisco(5) == 5
+
+def test_get_TrianguloAsteriscos_2():
+    assert C1.TrianguloAsterisco(-3) == 0
+
+def test_get_TrianguloAsteriscos_3():
+    assert C1.TrianguloAsterisco(5.5) == 5
