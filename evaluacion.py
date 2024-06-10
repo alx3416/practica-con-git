47b7b06df0a51bc6daf2736cdeee8483ee7b6244
Sergio Herrera <rsergio.h777@gmail.com>
2024-06-10 17:43:36 -0600

    Añadir problemas de evaluacion

--- evaluacion.py
+++ evaluacion.py
@@ -3,19 +3,19 @@ import numpy as np
 
 # Realice una función que calcule el área de un cuadrado
 def get_area_square(side):
-    area = None
+    area = side ** 2
     return area
 
 
 # Realice una función que calcule la hipotenusa
 def get_hypotenuse(side1, side2):
-    hypotenuse = None
+    hypotenuse = (side1 ** 2) + (side2 ** 2)
     return hypotenuse
 
 
 # Obtenga el vector en orden inverso
 def reverse_list(vector):
-    result = vector
+    result = vector[::-1]
     return result
 
 
@@ -23,27 +23,46 @@ def reverse_list(vector):
 # de una lista mayores a un valor umbral
 def get_indexes(vector, threshold):
     indexes = []
+    indexed_list = enumerate(vector)
+    for index, item in indexed_list:
+        if item > threshold:
+            indexes.append(index)
     return indexes
 
 
 # Obtenga el valor máximo de una matriz
 def get_max_value_from(matrix):
-    max_value = None
+    max_value = matrix.max()
     return max_value
 
 
 # Obtenga los valores diagonales de una matriz
 def get_diagonal_values_from(matrix):
-    diagonal_values = []
+    diagonal_values = matrix.diagonal()
     return diagonal_values
 
 # Elimine los valores adyacentes a todo valor NaN
 # INPUT = [6 10 5 8 9 NaN 23 9 7 3 21 43 NaN 4 6 7 8]
 # OUTPUT = [6 10 5 8 9 7 3 21 6 7 8]
+def get_adyacent_values(input_list, pivot):
+    output_list = input_list.copy()
+    indexed_list = enumerate(input_list)
+    pivot_indices = [index for index, item in indexed_list if item == pivot]
+    #print(pivot_indices)
+    for index in pivot_indices[::-1]:
+        output_list.pop(index + 1)
+        output_list.pop(index)
+        output_list.pop(index - 1)
+    return output_list
 
 # Replique los elementos de un vector
 # n=2, A=[1 2 3] -> [1 1 2 2 3 3]
 # n=0, A=[2 1] -> []
+def replicate_elements(vector, n):
+    if n <= 0:
+        return []
+    else:
+        output_list = [item1 for item2 in range(n+1) for item1 in [item2]*n]
 
 # Extraiga el primer digito distinto a cero, de cada valor en un vector
 # x = [1 0.3 -2 0.001 -0.0006, 582398, 3020]
