1712cfcd2ceec465324f21ea34a4c98c8ac8256b
PhdLalo <eduardo.garcia1511@gmail.com>
2024-06-30 12:18:39 -0600

    Update evaluacion.py

--- evaluacion.py
+++ evaluacion.py
@@ -1,54 +1,42 @@
-import numpy as np
+print("resultado1",resultado2)
+print("\n ")
 
-
-# Realice una función que calcule el área de un cuadrado
-def get_area_square(side):
-    area = None
-    return area
-
-
-# Realice una función que calcule la hipotenusa
-def get_hypotenuse(side1, side2):
-    hypotenuse = None
-    return hypotenuse
-
-
-# Obtenga el vector en orden inverso
-def reverse_list(vector):
-    result = vector
-    return result
-
-
-# Obtenga la posición o índice de los elementos
-# de una lista mayores a un valor umbral
-def get_indexes(vector, threshold):
-    indexes = []
-    return indexes
-
-
-# Obtenga el valor máximo de una matriz
-def get_max_value_from(matrix):
-    max_value = None
-    return max_value
-
-
-# Obtenga los valores diagonales de una matriz
-def get_diagonal_values_from(matrix):
-    diagonal_values = []
-    return diagonal_values
-
-# Elimine los valores adyacentes a todo valor NaN
-# INPUT = [6 10 5 8 9 NaN 23 9 7 3 21 43 NaN 4 6 7 8]
-# OUTPUT = [6 10 5 8 9 7 3 21 6 7 8]
-
-# Replique los elementos de un vector
-# n=2, A=[1 2 3] -> [1 1 2 2 3 3]
-# n=0, A=[2 1] -> []
 
 # Extraiga el primer digito distinto a cero, de cada valor en un vector
 # x = [1 0.3 -2 0.001 -0.0006, 582398, 3020]
 # y = [1 3 2 1 6 5 3]
 
+def extraer_primer_digito(vector):
+    digitos = []
+    for numero in vector:
+        str_num = str(numero).lstrip('-')  # Elimina el signo negativo si existe
+        for char in str_num:
+            if char.isdigit() and char != '0':
+                digitos.append(int(char))
+                break
+    return digitos
+x = [1, 0.3, -2, 0.001, -0.0006, 582398, 3020]
+y =  [1, 3, 2, 1, 6, 5, 3,]
+
+print("Lista original 1", x)
+print("Digito distinto de cero 1", extraer_primer_digito(x))
+print("Lista original 2", x)
+print("Digito distinto de cero 2", extraer_primer_digito(y))
+print("\n ")
 # Elimine los elementos redundantes en un vector, manteniendo el orden original
 # in = [5 3 6 4 7 7 3 5 9]
 # out = [5 3 6 4 7 9]
+
+def eliminar_redundantes(vector):
+    resultado = []
+    vistos = set()
+    for elemento in vector:
+        if elemento not in vistos:
+            resultado.append(elemento)
+            vistos.add(elemento)
+    return resultado
+
+vector = [5, 3, 6, 4, 7, 7, 3, 5, 9]
+resultado = eliminar_redundantes(vector)
+print("Vector original",vector)
+print("resultado",resultado)
